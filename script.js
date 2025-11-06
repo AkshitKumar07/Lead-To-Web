3a7c4ed1230a112d1429627e76da2febed98d462
AkshitKumar07 <kumar.akshat000@gmail.com>
2025-11-06 14:08:00 +0530

    script changes

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function handleSubmit() {
- let outputdate = document.querySelector(".outputdate");
+let capthachecked = false;
+
+function handleSubmit(event) {
+  if(capthachecked) {
+  let outputdate = document.querySelector(".outputdate");
   let inputdate = document.querySelector(".inputdate");
 
   //string ---- date (en_In)
 
   let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
   outputdate.value = formattedDate;
-
+  } else {
+  alert("Please check reCAPTCHA box to submit this lead");
+  event.preventDefault();
+}
 }
 
+  function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+      var elems = JSON.parse(
+        document.getElementsByName("captcha_settings")[0].value
+      );
+      elems["ts"] = JSON.stringify(new Date().getTime());
+      document.getElementsByName("captcha_settings")[0].value =
+        JSON.stringify(elems);
+    }
+  }
+  setInterval(timestamp, 500);
