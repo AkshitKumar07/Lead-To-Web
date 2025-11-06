117a41e78feda468836e8d81f2cd9c451a2f3f01
AkshitKumar07 <kumar.akshat000@gmail.com>
2025-11-06 15:27:15 +0530

    recaptcha

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 let capthachecked = false;
 
 function handleSubmit(event) {
-  if(capthachecked) {
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
+  if (capthachecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
 
-  //string ---- date (en_In)
+    //string ---- date (en_In)
 
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-  outputdate.value = formattedDate;
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formattedDate;
   } else {
-  alert("Please check reCAPTCHA box to submit this lead");
-  event.preventDefault();
-}
-function recaptchaSuccess() {
-  capthachecked = true;
-}
-
-}
-
-  function timestamp() {
-    var response = document.getElementById("g-recaptcha-response");
-    if (response == null || response.value.trim() == "") {
-      var elems = JSON.parse(
-        document.getElementsByName("captcha_settings")[0].value
-      );
-      elems["ts"] = JSON.stringify(new Date().getTime());
-      document.getElementsByName("captcha_settings")[0].value =
-        JSON.stringify(elems);
-    }
+    alert("Please check reCAPTCHA box to submit this lead");
+    event.preventDefault();
   }
-  setInterval(timestamp, 500);
+  function recaptchaSuccess() {
+    capthachecked = true;
+  }
+}
+
+function timestamp() {
+  var response = document.getElementById("g-recaptcha-response");
+  if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(
+      document.getElementsByName("captcha_settings")[0].value
+    );
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value =
+      JSON.stringify(elems);
+  }
+}
+setInterval(timestamp, 500);
